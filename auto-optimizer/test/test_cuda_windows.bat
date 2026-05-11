3c188afdb8331b7b02b8ac0074d1c3ed6ccdfd36
physicsdolphin <qtyelw@gmail.com>
2026-05-11 14:54:18 +0800

    fixed autoscan path failure since now we use python project in monorepo root dir and uv venv is also in root dir

--- auto-optimizer/test/test_cuda_windows.bat
+++ auto-optimizer/test/test_cuda_windows.bat
@@ -3,4 +3,4 @@ for /f %%a in ('echo prompt $E^| cmd') do set "ESC=%%a"
 
 set /a "new_duration=%~2 * 5"
 
-..\cli-stressor-cuda\.venv\scripts\python ..\cli-stressor-cuda\test.py --precisions fp16 --matrix-sizes 2048,4096,8192 --duration %new_duration%
+..\.venv\scripts\python ..\cli-stressor-cuda\test.py --precisions fp16 --matrix-sizes 2048,4096,8192 --duration %new_duration%
--- auto-optimizer/test/test_opencl_windows.bat
+++ auto-optimizer/test/test_opencl_windows.bat
@@ -3,4 +3,4 @@ for /f %%a in ('echo prompt $E^| cmd') do set "ESC=%%a"
 
 set /a "new_duration=%~2 * 5"
 
-..\cli-stressor-opencl\.venv\scripts\python ..\cli-stressor-opencl\test.py --precisions fp32 --matrix-sizes 2048,4096,8192 --duration %new_duration% --platform-index 0 --device-index 0
+..\.venv\scripts\python ..\cli-stressor-opencl\test.py --precisions fp32 --matrix-sizes 2048,4096,8192 --duration %new_duration% --platform-index 0 --device-index 0
